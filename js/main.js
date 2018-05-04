8ea4f0db26cf6c26b63282f3fce48548385079ca
lubeka <lubekadev@gmail.com>
2018-05-04 13:43:34 -0700

    Touching up skills section

--- js/main.js
+++ js/main.js
@@ -11,6 +11,27 @@ $(document).ready(function() {
     });
 
     $('.work--slider').slick({
-        slidesToShow: 3
+        slidesToShow: 3,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 });
